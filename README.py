c1c7be809c630acd2f26585f3baf4dc9e13368dc
Subhomoy23 <72864126+Subhomoy23@users.noreply.github.com>
2021-06-20 22:33:11 +0530

    Update README.py

--- README.py
+++ README.py
@@ -1,13 +1,14 @@
 # text-to-speech.py
+# install pyttsx3 and PyPDF2 from terminal section.
 this code will help to read your pdf so that u ca relax and listen your book.
 import pyttsx3
 import PyPDF2
-book = open('mad.pdf', 'rb')
+book = open('your pdf here.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 print(pages)
 speaker = pyttsx3.init()
-page = pdfReader.getPage()
+page = pdfReader.getPage(page no.)
 text = page.extractText()
 speaker.say(text)
 speaker.runAndWait()
